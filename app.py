8c9fc19b0bcd0913d41b333914f137ee1cb95944
shishirhebbar1 <114677714+shishirhebbar1@users.noreply.github.com>
2025-04-16 17:30:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ st.set_page_config(page_title="University Dashboard", layout="wide")
 st.title("🎓 University Data Dashboard")
 
 # ---- DATABASE CONNECTION ---- #
-connection_string = ("postgresql://postgres:Shishir25@"
-                     "[2600:1f18:2e13:9d10:2d4:d339:8d93:c938]:5432/postgres?sslmode=require")
+connection_string = ("postgresql://neondb_owner:npg_29nVFEUzhJkS@ep-red-mouse-a4kj0xwr-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require")
 
 def get_connection():
     return psycopg2.connect(connection_string)
